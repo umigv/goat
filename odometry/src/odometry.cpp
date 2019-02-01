08d994cb919d0c57271e23a942a7cbca89d0603b
Sean Oliver <snoliver@umich.edu>
2019-01-31 19:20:12 -0500

    call rosservice using model

--- odometry/src/odometry.cpp
+++ odometry/src/odometry.cpp
@@ -1,15 +1,24 @@
 #include "ros/ros.h"
 #include <nav_msgs/Odometry.h>
 #include <gazebo_msgs/GetModelState.srv>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "odometry");
-    ros::nodeHandle hn("~");
+    ros::nodeHandle nh("~");
     ros::Publisher pub = nh.advertise<nav_msgs::Odometry>("filtered", 1000);
     ros::Service::waitForService("gazebo/get_model_state", -1)
-    ros::ServiceClient client = nh.ServiceClient<nav_msgs::Odometry>("gazebo/get_model_state")
+    ros::ServiceClient client = nh.ServiceClient<gazebo_msgs::ModelState>("gazebo/get_model_state")
     
     gazebo_msgs::ModelState model = GetModelState::GetModelStateRequest()
     model.model_name = "mybot";
 
+    ros::Rate r(50.0);
+    while(nh.ok()) {
+        srv_model = client.call(model);
+        if(model.success) {
+            
+        }
+    }
 }
