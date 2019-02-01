0365c277578d32f443647693200c0b63f8ec102f
Sean Oliver <snoliver@umich.edu>
2019-01-31 19:11:11 -0500

    get model from gazebo

--- odometry/src/odometry.cpp
+++ odometry/src/odometry.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include <nav_msgs/Odometry.h>
+#include <gazebo_msgs/GetModelState.srv>
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "odometry");
@@ -8,4 +9,7 @@ int main(int argc, char** argv) {
     ros::Service::waitForService("gazebo/get_model_state", -1)
     ros::ServiceClient client = nh.ServiceClient<nav_msgs::Odometry>("gazebo/get_model_state")
     
+    gazebo_msgs::ModelState model = GetModelState::GetModelStateRequest()
+    model.model_name = "mybot";
+
 }
