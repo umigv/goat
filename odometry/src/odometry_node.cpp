77950b59c1426acfe5742e7c5ccd828896ddcd47
Sean Oliver <snoliver@umich.edu>
2019-02-10 16:59:26 -0500

    added ROS_ERRORS for debug

--- odometry/src/odometry_node.cpp
+++ odometry/src/odometry_node.cpp
@@ -2,6 +2,7 @@
 #include "nav_msgs/Odometry.h"
 #include "gazebo_msgs/GetModelState.h"
 #include "gazebo_msgs/ModelState.h"
+#include <iostream>
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "odometry");
@@ -9,19 +10,33 @@ int main(int argc, char** argv) {
     ros::Publisher pub = nh.advertise<nav_msgs::Odometry>("filtered", 1000);
     ros::service::waitForService("gazebo/get_model_state", -1); // timeout?
     ros::ServiceClient client = nh.serviceClient<gazebo_msgs::GetModelState>("gazebo/get_model_state");
-    
+    if(!client.isValid()){
+        ROS_ERROR("Service Handle is not valid");
+        exit(1);
+    }
     gazebo_msgs::GetModelStateRequest model_req;
     model_req.model_name = "mybot";
     gazebo_msgs::GetModelStateResponse model_rep;
 
     ros::Rate r(50.0); //more than 50 Hz?
     while(nh.ok()) {
-        client.call(model_req, model_rep);
-        nav_msgs::Odometry odom;
-        odom.pose.pose = model_rep.pose;
-        odom.twist.twist = model_rep.twist;
+        if(client.call(model_req, model_rep)) {
+            if(model_rep.success) {
+                nav_msgs::Odometry odom;
+                odom.pose.pose = model_rep.pose;
+                odom.twist.twist = model_rep.twist;
 
-        pub.publish(odom);
+                pub.publish(odom);
+            }
+            else {
+                ROS_ERROR("Call succeded, but returned failed response");
+                exit(1);
+            }
+        }
+        else{
+            ROS_ERROR("Call failed");
+            exit(1);
+        }
 	    r.sleep();
     }
 }
