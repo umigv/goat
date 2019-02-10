fed54765c29cbbd94f4244727d43799ead8c8f31
Sean Oliver <snoliver@umich.edu>
2019-02-10 16:12:02 -0500

    made it compile

--- odometry/src/odometry_node.cpp
+++ odometry/src/odometry_node.cpp
@@ -1,27 +1,27 @@
 #include "ros/ros.h"
-#include <nav_msgs/Odometry.h>
-#include <gazebo_msgs/GetModelState.srv>
+#include "nav_msgs/Odometry.h"
+#include "gazebo_msgs/GetModelState.h"
+#include "gazebo_msgs/ModelState.h"
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "odometry");
-    ros::nodeHandle nh("~");
+    ros::NodeHandle nh("~");
     ros::Publisher pub = nh.advertise<nav_msgs::Odometry>("filtered", 1000);
-    ros::Service::waitForService("gazebo/get_model_state", -1) // timeout?
-    ros::ServiceClient client = nh.ServiceClient<gazebo_msgs::ModelState>("gazebo/get_model_state")
+    ros::service::waitForService("gazebo/get_model_state", -1); // timeout?
+    ros::ServiceClient client = nh.serviceClient<gazebo_msgs::GetModelState>("gazebo/get_model_state");
     
-    gazebo_msgs::ModelState model = GetModelState::GetModelStateRequest()
-    model.model_name = "mybot";
+    gazebo_msgs::GetModelStateRequest model_req;
+    model_req.model_name = "mybot";
+    gazebo_msgs::GetModelStateResponse model_rep;
 
     ros::Rate r(50.0); //more than 50 Hz?
     while(nh.ok()) {
-        srv_model = client.call(model);
-        if(model.success) {
-            nav_msgs::Odometry odom(); //ctors?
-            odom.pose.pose = model.Pose;
-            odom.twist.twist = model.twist;
+        client.call(model_req, model_rep);
+        nav_msgs::Odometry odom;
+        odom.pose.pose = model_rep.pose;
+        odom.twist.twist = model_rep.twist;
 
-            pub.publish(odom);
-        }
-        // exception handling?
+        pub.publish(odom);
+	    r.sleep();
     }
 }
