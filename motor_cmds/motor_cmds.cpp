3fcf5ed687fe2ac8a26f2ee12512fa26fd3dd656
Sean Oliver <snoliver@umich.edu>
2019-04-10 19:03:11 -0400

    implimented face target and step functions

--- motor_cmds/motor_cmds.cpp
+++ motor_cmds/motor_cmds.cpp
@@ -2,8 +2,35 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Pose.h>
-#include <geometry_msgs/Quaternion.>
-#include <vector> 
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Pose2d.h>
+#include<tf/tf.h>
+#include <vector>
+
+tf::Vector3 get_heading(const geometry_msgs::Quaternion& quaternion){
+    tf::Matrix3x3 rotation_matrix{quaternion};
+    //TODO get heading vector from quaternion
+    return yaw;
+}
+
+void face_towards(const geometry_msgs::Point& target, geometry_msgs::Point& pos, geometry_msgs::Quaternion& orientation, ros::Publisher& pub){
+    double angle = 100; // change to constant
+    while(angle > threshold || angle < -threshold){
+        tf::Vector3& heading = quaternion_to_yaw(orientation);
+        tf::Vector3 target_heading = tf::Vector3{target.x, target.y, target.z} - tf::Vector3{pos.x, pos.y, pos.z};
+        angle = heading.angle(target_heading);
+        geometry_msgs::Twist t;
+        t.angular.z = angle/scale; // change scale to value
+        pub.publish(t);
+    }
+    pub.publish(geometry_msgs::Twist{});
+}
+
+void step(geometry_msgs::Quaternion& orientation, ros::Publisher& pub){
+    geometry_msgs::Twist t;
+    t.linear = get_heading(orientation);
+    pub.publish(t);
+}
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "motor_cmds"); //good node name?
@@ -23,12 +50,12 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    auto callback = [&path = std::as_const(path), &it, &current_pos = std::as_const(current_pos), 
-        &current_orientation = std::as_const(current_orientation), &pub](const ros::TimerEvent& event){
+    auto callback = [&path = std::as_const(path), &it, &current_pos, 
+        &current_orientation, &pub](const ros::TimerEvent& event){
             while(*it != path.end()){
                 if(target_ahead(*it, current_pos, current_orientation)){ //watch for edge case of on top of *it
-                    face_towards(*it);
-                    step();
+                    face_towards(*it, current_pos, current_orientation, pub);
+                    step(current_orientation, pub);
                     return;
                 }
                 it++;
