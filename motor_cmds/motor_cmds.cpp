2ef62cfaaf61518c4b7d44a0c9e72d521409dd12
Sean Oliver <snoliver@umich.edu>
2019-04-14 17:29:15 -0400

    bug fix

--- motor_cmds/motor_cmds.cpp
+++ motor_cmds/motor_cmds.cpp
@@ -28,7 +28,8 @@ bool target_ahead(const geometry_msgs::Point& target, geometry_msgs::Point& pos,
 }
 
 tf::Vector3 get_heading(const geometry_msgs::Quaternion& quaternion){
-    return tf::Matrix3x3{quaternion}*INITIAL_FACING;
+    tf::Matrix3x3 rotation_matrix{quaternion};
+    return rotation_matrix*INITIAL_FACING;
 }
 template <typename t>
 int signum(T val){
@@ -41,32 +42,35 @@ void face_towards(const geometry_msgs::Point& target, geometry_msgs::Point& pos,
         geometry_msgs::Twist t;
         t.angular.z = signum(angle)*ROTATION_MAGNITUDE;
         pub.publish(t);
-    } while(angle > threshold || angle < -1*threshold);
+    } while(angle > ANGLE_THRESHOLD || angle < -1*ANGLE_THRESHOLD);
     pub.publish(geometry_msgs::Twist{}); //publish default twist to halt
 }
 
 void step(geometry_msgs::Quaternion& orientation, ros::Publisher& pub){
     geometry_msgs::Twist t;
-    t.linear = get_heading(orientation).normalized()*LINEAR_MAGNITUDE;
+    const tf::Vector3 norm_heading = get_heading(orientation).normalized()*LINEAR_MAGNITUDE;
+    t.linear.x = norm_heading.getX();
+    t.linear.y = norm_heading.getY();
+    //ignore z?
     pub.publish(t);
 }
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "motor_cmds"); //good node name?
-    ros::nodeHandle nh("~");
+    ros::NodeHandle nh("~");
     ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
-    ros::Subsriber sub = nh.subscribe("Pose", 1000, callback_getPose); // need to update topic name 
 
-    std::vector<geometry_msgs::Point> path = //what A* passes to us, maybe not copy?
-    auto it target = path.begin();
+    std::vector<geometry_msgs::Point> path;//what A* passes to us, maybe not copy?
+    auto target_it = path.begin();
     geometry_msgs::Point current_pos; //current robot position passed from Sensors
     geometry_msgs::Quaternion current_orientation; //also passed from Sensors
 
     // callback can not take params, it has to be in main unless created as a class member function
-    void callback_getPose(geometry_msgs::Pose msg) {
+    void callback_getPose = [&current_pos, &current_orientation](geometry_msgs::Pose msg) {
         current_pos = msg.position;
         current_orientation = msg.orientation;
-    }  
+    };
+    ros::Subscriber sub = nh.subscribe("Pose", 1000, callback_getPose); // need to update topic name 
 
     double time_step;
     if(!nh.getParam("time_step", time_step)){ //maybe make a default value?
@@ -76,22 +80,24 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    auto callback = [&path = std::as_const(path), &it, &current_pos, 
-        &current_orientation, &pub](const ros::TimerEvent& event){
-            while(*it != path.end()){
-                if(target_ahead(*it, current_pos, current_orientation)){ //watch for edge case of on top of *it
-                    face_towards(*it, current_pos, current_orientation, pub);
+    auto callback = [&](const ros::TimerEvent& event){
+            while(target_it != path.end()){
+                if(target_ahead(*target_it, current_pos, current_orientation)){ //watch for edge case of on top of *it
+                    face_towards(*target_it, current_pos, current_orientation, pub);
                     step(current_orientation, pub);
-                    return;
+                    
                 }
-                it++;
+                else{
+                    target_it++;
+                }
             }
             ROS_INFO_STREAM("No More Targets Ahead");
             ros::shutdown();
             ros::waitForShutdown();
             return 1;
-        }
-
-    ros::Timer timer = nh.createTimer(ros::Duration(time_step), callback);
-    ros::spin();
+        };
+    while(ros::ok()){
+        ros::Timer timer = nh.createTimer(ros::Duration(time_step), callback);
+        ros::spin();
+    }
 }
