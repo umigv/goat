ef9dcf6432ac9a27ae574e8b62af8cc8ff4feba1
trsabb <trsabb@umich.edu>
2019-05-28 19:33:46 -0400

    Added member function to check each adjacent state

--- path_planning/goatcontrol_integrated/a_star_test.cpp
+++ path_planning/goatcontrol_integrated/a_star_test.cpp
@@ -1,4 +1,4 @@
-#include "a_star_test.h"
+##include "a_star_test.h"
 
 using namespace std;
 
@@ -16,7 +16,7 @@ a_star_test::a_star_test(int width, int height, position strt, position trg, std
     cost_map = costs;
 }
 
-// Breadth first search 
+// Breadth first search
 bool a_star_test::make_reachable_collection()
 {
     backtrack_map.resize(costmap_height,
@@ -28,94 +28,73 @@ bool a_star_test::make_reachable_collection()
         position current = open_set.top();
         // cout << "Current position: " << current.x << " " << current.y << '\n';
         open_set.pop();
-
+        
         // if it is the target, exit the loop
         if(current == target)
         {
             return true;
         } // if
-
+        
         if(closed_set.find(current) != closed_set.end()){
             continue; // skip this iteration of the loop
         }
-
+        
         closed_set.insert(current);
-
+        
         // else add all adjacent states to the queue
-		unsigned int x = current.x;
-		unsigned int y = current.y;
-
-		// add southeast state
-    	position southeast = position(x + 1, y + 1, current);
-    	// but only if it is within the bounds of the cost_map,
-    	// reachable (cost_map val < 200), and it hasn't been added already
-	    if((x + 1 < costmap_height) && (y + 1 < costmap_width)
-           && cost_map[x + 1][y + 1] < 200
-           && closed_set.find(southeast) == closed_set.end())
-	    {
-			open_set.push(southeast);
-			backtrack_map[x + 1][y + 1] = current;
-	    }
-
-		position east = position(x + 1, y, current);
-		if((x + 1 < costmap_height) && cost_map[x + 1][y] < 200
-            && closed_set.find(east) == closed_set.end())
-		{
-			open_set.push(east);
-			backtrack_map[x + 1][y] = current;
-		}
-
-		position northeast = position(x + 1, y - 1, current);
-		if((x + 1 < costmap_height) && y >= 1 && cost_map[x + 1][y - 1]  < 200
-            && closed_set.find(northeast) == closed_set.end())
-		{
-			open_set.push(northeast);
-			backtrack_map[x + 1][y - 1] = current;
-		}
-
-		position south = position(x, y + 1, current);
-		if((y + 1 < costmap_width) && cost_map[x][y + 1] < 200
-            && closed_set.find(south) == closed_set.end())
-		{
-			open_set.push(south);
-			backtrack_map[x][y + 1] = current;
-		}
-
-		position north = position(x, y - 1, current);
-		if(y >= 1 && cost_map[x][y - 1] < 200
-            && closed_set.find(north) == closed_set.end())
-		{
-			open_set.push(north);
-			backtrack_map[x][y - 1] = current;
-		}
-
-		position southwest = position(x - 1, y + 1, current);
-		if(x >= 1 && (y + 1 < costmap_width) && cost_map[x - 1][y + 1] < 200
-            && closed_set.find(southwest) == closed_set.end())
-		{
-			open_set.push(southwest);
-			backtrack_map[x - 1][y + 1] = current;
-		}
-
-		position west = position(x - 1, y, current);
-		if(x >= 1 && cost_map[x - 1][y] < 200
-            && closed_set.find(west) == closed_set.end())
-		{
-			open_set.push(west);
-			backtrack_map[x - 1][y] = current;
-		}
-
-		position northwest = position(x - 1, y - 1, current);
-		if(x >= 1 && y >= 1 && cost_map[x - 1][y - 1] < 200
-            && closed_set.find(northwest) == closed_set.end())
-		{
-			open_set.push(northwest);
-			backtrack_map[x - 1][y - 1] = current;
-		}
+        unsigned int x = current.x;
+        unsigned int y = current.y;
+        
+        // add southeast state
+        position southeast = position(x + 1, y + 1, current);
+        check_adjacent(southeast, current);
+        
+        // add east state
+        position east = position(x + 1, y, current);
+        check_adjacent(east, current);
+        
+        // add northeast state
+        position northeast = position(x + 1, y - 1, current);
+        check_adjacent(northeast, current);
+        
+        // add south state
+        position south = position(x, y + 1, current);
+        check_adjacent(south, current);
+        
+        // add north state
+        position north = position(x, y - 1, current);
+        check_adjacent(north, current);
+        
+        // add southwest state
+        position southwest = position(x - 1, y + 1, current);
+        check_adjacent(southwest, current);
+        
+        // add west state
+        position west = position(x - 1, y, current);
+        check_adjacent(west, current);
+        
+        // add northwest state
+        position northwest = position(x - 1, y - 1, current);
+        check_adjacent(northwest, current);
     } // while
-
+    
     return false;
 } // make_reachable_collection
+
+// Check adjacent state
+void a_star_test::check_adjacent(position &p, position &c)
+{
+    // add only if it is within the bounds of the cost_map,
+    // reachable (cost_map val < 200), and it hasn't been added already
+    if((p.x < costmap_height) && (p.y < costmap_width) &&
+       (p.x >= 0) && (p.y >= 0)
+       && cost_map[p.x][p.y] < 200
+       && closed_set.find(p) == closed_set.end())
+    {
+        open_set.push(p);
+        backtrack_map[p.x][p.y] = c;
+    }
+}
 
 // Starts at target and backtracks, adding information to the solution path
 // Then once it hits the starting position, stops
@@ -123,7 +102,7 @@ void a_star_test::backtracker(vector<position> &solution_path)
 {
     unsigned int x = target.x;
     unsigned int y = target.y;
-
+    
     while(x != start.x || y != start.y)
     {
         solution_path.push_back(position(x, y));
@@ -141,4 +120,3 @@ double a_star_test::distance(position a, position b)
     double y_diff = a.y - b.y;
     return sqrt((x_diff * x_diff) + (y_diff * y_diff));
 }
-
