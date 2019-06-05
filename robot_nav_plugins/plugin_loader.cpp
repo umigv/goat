a7b9a1611d1909686325167a7693b4c6aa563f46
trsabb <trsabb@umich.edu>
2019-06-04 21:50:15 -0400

    Added correct namespaces and class names for nav_core_adapter plugin usage

--- robot_nav_plugins/plugin_loader.cpp
+++ robot_nav_plugins/plugin_loader.cpp
@@ -1,19 +1,18 @@
 #include <pluginlib/class_loader.h>
+// fix this next line
 #include <polygon_interface_package/polygon.h>
 
-//... some code ...
-
-pluginlib::ClassLoader<polygon_namespace::Polygon> poly_loader("polygon_interface_package", "polygon_namespace::Polygon");
+// ... some code ...
+pluginlib::ClassLoader<nav_core_adapter::CostmapAdapter> costmap_loader("nav_core2::Costmap", "nav_core_adapter::CostmapAdapter");
 
 try
 {
-boost::shared_ptr<polygon_namespace::Polygon> poly = poly_loader.createInstance("rectangle_namespace::Rectangle");
-
-//... use the polygon, boost::shared_ptr will automatically delete memory when it goes out of scope
+boost::shared_ptr<nav_core_adapter::CostmapAdapter> costmap = costmap_loader.createInstance("nav_core2::Costmap");
+// ... use the costmap, boost::shared_ptr will automatically delete memory when it goes out of scope
 }
 
 catch(pluginlib::PluginlibException& ex)
 {
-//handle the class failing to load
+// handle the class failing to load
 ROS_ERROR("The plugin failed to load for some reason. Error: %s", ex.what());
 }
